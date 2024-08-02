4b72ffadb0d678d8f086709962632c47fba15c47
Sophors Pheng <sophorspheng.num@gmail.com>
2024-08-03 01:23:12 +0700

    deploy

--- index.js
+++ index.js
@@ -19,8 +19,8 @@ app.get("/",(req,res)=>{
 
 require("./routes/customerRoutes")(app)
 
-// const port = 8080 
-// app.listen(port,()=>{
-//     console.log("http://localhost:"+port)
-// })
+const port = 8080 
+app.listen(port,()=>{
+    console.log("http://localhost:"+port)
+})
 module.exports = app;
